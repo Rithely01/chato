bd46746bdc4e3fe23acc22eee7599cf86958cdd7
unknown <rithelynunes6213@digitalcollege.internal>
2025-06-24 10:58:07 -0300

    aula chataa

---
+++ repositorio.js
@@ -0,0 +1,57 @@
+// for (let i =0; i <= 5; i++) {
+//     console.log('número $(i)');
+// }
+
+// console.log ('fim do loop');
+//     const marcasDecarros = ['Honda','BYD', 'Toyta','Jeep'];
+//     const itensDoArray = marcasDecarros.length;
+//     cont frase = "Rodrigo Madeira";
+//     console.log(itensDoArray);
+//     console.log(frase.length);
+//     for (let i = 0; i < marcasDecarros.)
+
+//    ;console.log ('fim do loop');
+//     const marcasDecarros = ['Honda,'BYD', 'Toyta','Jeep'];
+//     const itensDoArray = marcasDecarros.length;
+//     cont frase = "Rodrigo Madeira";
+//     console.log(itensDoArray);
+//     console.log(frase.length);
+//     for (let i = 0; i < marcasDecarros. )
+const tenis = [
+    {
+    marca: "Nike",
+    modelo: "Air Max",
+    cor:"preto",
+    tamanho:[42, 43, 44],
+    preço: 499.99,
+    estoque: 10,
+},
+{  marca: "Adids",
+    modelo: "Utrapassado",
+    cor:"vermelho",
+    tamanho:[37, 38, 39],
+    preço: 599.99,
+    estoque: 5,
+    
+},
+{  marca: "Puma",
+    modelo: "RS-X",
+    cor:"azul",
+    tamanho:[38, 39, 40],
+    preço: 399.99,
+    estoque: 8,
+    
+},
+{  marca: "Reebok",
+    modelo: "Classic",
+    cor:"cinza",
+    tamanho:[39, 40, 41],
+    preço: 299.99,
+    estoque: 12,
+    
+},
+]
+
+for(let i = 0; i < tenis.length ; i++){
+    tenis {i}.estoque = tenis
+}
